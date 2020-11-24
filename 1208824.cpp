771333a7d2e015eb32516ede5edb5c9f9f7287e9
Alexander Deider <arth.medintech@gmail.com>
2020-11-24 17:12:19 +0700

    [1208824] Hyper-prefix sums — repair overflows and correctness of modular ariphmetics

--- 1208824.cpp
+++ 1208824.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdint>
 
 const auto MOD = 998244353;
 
@@ -13,84 +14,94 @@ void print(int* arr, const int n, const char *glue=" ") {
 void solve(int* arr, const int n, int k) {
   for (auto r = 0; r < k; ++r) {
     for (auto i = 0; i < n-1; ++i) {
-      arr[i+1] = (arr[i] + arr[i+1]) % MOD;
+      arr[i+1] = (int)((arr[i] + arr[i+1]) % MOD);
     }
-    // std::cout << r+1 << ": "; print(arr, n, "\t");
   }
 }
+int64_t gcd_ex(uint64_t a, uint64_t b, int64_t &x, int64_t &y) {
+  if (a == 0) {
+    x = 0; y = 1;
+    return b;
+  }
+  int64_t x1, y1, t;
+  int64_t d = gcd_ex((b+a) % a, a, x1, y1);
+  t = (b / a) * x1;
+  x = y1 - t;
+  y = x1;
 
-void solve_mul(int* arr, const int n, int k) {
-    for (auto i = 1; i < n; ++i) {
-      arr[i] = (int)((long long)arr[i-1]*k + arr[i]) % MOD;
-    }
+  return d;
+}
+
+uint64_t reciprocal_mod(uint64_t a, uint64_t m) { // a**-1 (mod m)
+  int64_t x{}, y{};
+  if (gcd_ex(a, m, x, y) == 1) {
+
+    return x < 0 ? x + m : x;
+  }
+  std::cerr << a << " and " << m << " are coprimes\n";
+  return 1; // no reciprocal
 }
 
 void solve_sqr(int* arr, const int n, int k) {
-  // fixme: Точность всё ещё теряется, не пойму где =)
+    // A_k_i = A_0_i + \sum_{j=1}^i -1^{j-1}* \frac{k!}{j!(k-j)!}*A_0_{(i-j)}
     for (int i = 1; i < n; ++i) {
-      unsigned long long term = k;
+      uint64_t term = k;
       auto sign = 1;
       for (int j = 1; j <= i; ++j) {
-        if (sign == 1) {
-          arr[i] =  (int)((long)(arr[i] + sign * (unsigned long long)(term % MOD * arr[i-j] % MOD)) % MOD);
-        } else {
-          arr[i] =  (int)((long)(arr[i] + sign * (unsigned long long)(term * arr[i-j])) % MOD);
-        }
-        term = (term * (k-j) / (j+1)) ;
-        sign = -sign;
-        while (arr[i] < 0) {
-          arr[i] = arr[i] + MOD;
-        }
+        // arr[i] += sign * term * arr[i-j];
+        // term = term * (k-j) / (j+1);
+        uint64_t t = term;
+        t = (t * sign) % MOD;
+        t = (t * arr[i-j]) % MOD;
+        arr[i] = (int)((arr[i] + t) % MOD);
+
+        term = (term * (k-j)) % MOD;
+        term = (term * reciprocal_mod(j+1, MOD)) % MOD;
+
+        sign = (sign == 1) ? MOD - 1 : 1;
       }
     }
 }
 
-void debug(int k, int n, int start, int mult) {
-  int *arr = new int[n];
-  int *A = new int[n];
-  for (int i = 0, d=start; i < n; ++i, d*=mult) {
-    arr[i] = d;
-    A[i] = d;
+void test_reciprocal_mod() {
+  if(reciprocal_mod(2, 27) != 14) {
+    std::cerr << "[FAIL] reciprocal_mod(2) is "<< reciprocal_mod(2, 27) <<", 14 expected" << std::endl;
   }
-  solve(arr, n, k);
-  solve_sqr(A, n, k);
-  print(arr, n);
-  print(A, n);
+  if(reciprocal_mod(2, MOD) != 499122177) {
+    std::cerr << "[FAIL] reciprocal_mod(2) is "<< reciprocal_mod(2, MOD) <<", 499122177 expected" << std::endl;
+  }
+  if(reciprocal_mod(7, MOD) != 855638017) {
+    std::cerr << "[FAIL] reciprocal_mod(7) is "<< reciprocal_mod(2, MOD) <<", 855638017 expected" << std::endl;
+  }
+  if(reciprocal_mod(18, MOD) != 720954255) {
+    std::cerr << "[FAIL] reciprocal_mod(18) is "<< reciprocal_mod(2, MOD) <<", 720954255 expected" << std::endl;
+  }
+  for (int i = 2; i < 10000; ++i) {
+    auto rec = reciprocal_mod(i, MOD);
+    auto res = ((uint64_t)rec * i) % MOD;
+    if (res != 1) {
+      std::cerr << "[FAIL] reciprocal_mod(" << i << ") * "<<i<<" = " << rec << "*"<<i<<"(mod M)="<< (rec*i)%MOD <<  ", 1 expected" << std::endl;
+    }
+  }
+}
 
-  delete[] arr;
-  delete[] A;
+void test_solve(void (*fn)(int*, const int, int), int n, int k, int start, int mult, int expected) {
+    int *A = new int[n];
+    for (int i = 0, d=start; i < n; ++i, d=(int)(((uint64_t)d*mult) % MOD)) {
+      A[i] = d;
+    }
+    fn(A, n, k);
+    if (A[n-1] != expected) {
+      std::cerr << "Last element is " << A[n-1] << "; " << expected << " expected\n";
+    }
+    delete[] A;
 }
 
 int main() {
-  int start = 5;
-  for (int k = 64; k <= 68; ++k) {
-    std::cout << "\nk = " << k << "; start=" << start << std::endl;
-    debug(k, 25, start, 5);
-  }
-
-  const int n = 20;
-  int k = 100;
-
-  int arr0[]{5, 25, 125, 625, 13, 17, 19, 23, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-  solve(arr0, n, k);
-  print(arr0, n);
-  // 3 5266946 586395883 760641300 100716517 676709075 11153447 256148068 149906432 237819898 589215151 630658815 656728361 66027592 551072302 318065800 839518503 598503241 909406756 741324981
-
-  int arr[]{5, 25, 125, 625, 13, 17, 19, 23, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-  solve_sqr(arr, n, k);
-  print(arr, n);
+  test_reciprocal_mod();
+  test_solve(solve, 100, 1e6, 1, 1, 428943052);
+  test_solve(solve_sqr, 100, 1e6, 1, 1, 428943052);
+  test_solve(solve_sqr, 2000, 1e9, 13, 7, 92923453);
+  test_solve(solve_sqr, 2000, 1e9, 1, 1, 93451579);
+  test_solve(solve_sqr, 2000, 1e9, 2, 1, 186903158);
 }
-/*
-12: 1 13 91 458 1856 6422 19656 54483 139074 331058 742118 1578824 3208036 6258448 11773996 21440094 37910187 65268135 109671705 180234210
-
-a[k, r] = a[k, r-1] + a[k-1, r]
-a[k, r] = a[k, r-1] + a[k-2, r] + a[k, r-1] - a[k, r-2]
-a[k, r] = 2*a[k, r-1] - a[k, r-2] + a[k-2, r]
-
-a[k, 0] = a[0, 0]
-a[k, 1] = a[k, 0] * k + a[0, 1]
-a[k, 2] = a[k, 1] * k - a[k, 0]*(k-1) + a[0, 2]
-
-
-
-*/
